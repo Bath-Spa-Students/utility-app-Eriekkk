eaac6e86a0e735bab1c56666dbab48ebfc6aa2c0
Eriekkk <erielpsalmgalura@gmail.com>
2024-01-07 22:39:11 +0400

    Vending Machine Utility app

---
+++ Vendine Machine.py
@@ -0,0 +1,228 @@
+#dictionary aff all the items that are sold in the vending machine
+bread = [#dictionary for all the bread items
+    {"Code":"A1", "Bread": "Pandesal", "Price": 1.50 },
+    {"Code":"A2", "Bread": "Ensaymada", "Price": 2.50 },
+    {"Code":"A3", "Bread": "Pan De Coco", "Price": 3.00 },
+    {"Code":"A4", "Bread": "Pan De Ube", "Price": 2.50 },
+    {"Code":"A5", "Bread": "Pan De Leche", "Price": 4.50 },
+    {"Code":"A6", "Bread": "Empanada", "Price": 4.50 },
+    {"Code":"A7", "Bread": "Egg Pie", "Price": 6.00 },
+    {"Code":"A8", "Bread": "Buko Pie", "Price": 8.00 },
+    {"Code":"A9", "Bread": "Monay", "Price": 3.50 },
+    {"Code":"A10", "Bread": "Hopia", "Price": 3.00 },
+]
+drink = [#dictionary for all the drink items
+     {"Code":"B1", "Drinks": "Coffee", "Price": 5.00 },
+     {"Code":"B2", "Drinks": "Hot Chocolate", "Price": 7.50 },
+     {"Code":"B3", "Drinks": "Tea", "Price": 1.00 },
+     {"Code":"B4", "Drinks": "Ginger Tea", "Price": 1.50 },
+     {"Code":"B5", "Drinks": "Espresso", "Price": 10.50 },
+     {"Code":"B6", "Drinks": "Water", "Price": 0.50 },
+     {"Code":"B7", "Drinks": "Soft Drink", "Price": 2.50 },
+     {"Code":"B8", "Drinks": "Smoothie", "Price": 10.00 },
+     {"Code":"B9", "Drinks": "Juice", "Price": 5.00 },
+     {"Code":"B10", "Drinks": "Ice Coffe", "Price": 12.00 },
+]
+fruits = [#dictionary for all the fruit items
+    {"Code":"C1", "Fruits": "Apple", "Price": 1.00 },
+    {"Code":"C2", "Fruits": "Banana", "Price": 1.00 },
+    {"Code":"C3", "Fruits": "Orange", "Price": 2.00 },
+    {"Code":"C4", "Fruits": "Watermelon", "Price": 9.00 },
+    {"Code":"C5", "Fruits": "Grapes", "Price": 3.50 },
+    {"Code":"C6", "Fruits": "Strawberries", "Price": 5.00 },
+    {"Code":"C7", "Fruits": "Pears", "Price": 2.00 },
+    {"Code":"C8", "Fruits": "Melon", "Price": 7.00 },
+    {"Code":"C9", "Fruits": "Kiwi", "Price": 5.00 },
+    {"Code":"C10", "Fruits": "Pineapple", "Price": 5.00 },
+]
+noodles = [#dictionary for the all noodle items
+    {"Code":"D1", "noodles": "Ramen", "Price": 10.50 },
+    {"Code":"D2", "noodles": "Udon", "Price": 13.00 },
+    {"Code":"D3", "noodles": "Chow Mein", "Price": 10.50 },
+    {"Code":"D4", "noodles": "Pancit Canton", "Price": 5.00 },
+    {"Code":"D5", "noodles": "Pancit Palabok", "Price": 9.50 },
+    {"Code":"D6", "noodles": "Soba", "Price": 8.00 },
+    {"Code":"D7", "noodles": "Flat Rice", "Price": 7.50 },
+    {"Code":"D8", "noodles": "Spaghetti", "Price": 11.00 },
+    {"Code":"D9", "noodles": "Samyang", "Price": 8.50 },
+    {"Code":"D10", "noodles": "Bulkad", "Price": 7.00 },
+]
+soup = [#dictionary fo the all soup items
+    {"Code":"E1", "soup": "Miso", "Price": 13.50},
+    {"Code":"E2", "soup": "Tomato", "Price": 8.00 },
+    {"Code":"E3", "soup": "Italian Wedding", "Price": 14.00 },
+    {"Code":"E4", "soup": "Minestrone", "Price": 7.40 },
+    {"Code":"E5", "soup": "Lentil", "Price": 6.50 },
+    {"Code":"E6", "soup": "French Onion", "Price": 10.00 },
+    {"Code":"E7", "soup": "Chicken Tortilla", "Price": 15.00 },
+    {"Code":"E8", "soup": "Butternut Squash", "Price": 13.50 },
+    {"Code":"E9", "soup": "Corn Cowder", "Price": 10.50 },
+    {"Code":"E10", "soup": "Mushroom", "Price": 5.50 },
+]
+ceareal = [#dictionary for all the cereal items
+    {"Code":"E1", "cereal": "Cheerios", "Price": 12.50 },
+    {"Code":"E2", "cereal": "Corn Flakes", "Price": 15.00 },
+    {"Code":"E3", "cereal": "Froot loops", "Price": 12.00 },
+    {"Code":"E4", "cereal": "Captain Crunch", "Price": 10.00 },
+    {"Code":"E5", "cereal": "Lucky Charms", "Price": 13.50 },
+    {"Code":"E6", "cereal": "General Mills", "Price": 10.00 },
+    {"Code":"E7", "cereal": "Nestle", "Price": 11.00 },
+    {"Code":"E8", "cereal": "Frosted Flakes", "Price": 13.00 },
+    {"Code":"E9", "cereal": "Apple Jacks", "Price": 10.00 },
+    {"Code":"E10", "cereal": "Rice Krispies", "Price": 11.50 },
+]
+chips = [#dictionary for all the chips items
+    {"Code":"F1", "chips": "Takis", "Price": 5.00 },
+    {"Code":"F2", "chips": "Doritos", "Price": 7.00 },
+    {"Code":"F3", "chips": "Lays", "Price": 7.00 },
+    {"Code":"F4", "chips": "Cheetos", "Price": 3.50 },
+    {"Code":"F5", "chips": "Pringles", "Price": 8.50 },
+    {"Code":"F6", "chips": "Tostitos", "Price": 10.00 },
+    {"Code":"F7", "chips": "Sun Chips", "Price": 5.00 },
+    {"Code":"F8", "chips": "Fritos", "Price": 7.00 },
+    {"Code":"F9", "chips": "Popchips", "Price": 5.00 },
+    {"Code":"F10", "chips": "Cape Cod", "Price": 5.50 },
+]
+other = [#dictionary for the other items
+    {"Code":"G1", "other": "French Fries", "Price": 10.00 },
+    {"Code":"G2", "other": "Chicken with Rice", "Price": 10.00 },
+    {"Code":"G3", "other": "Fried Egg", "Price": 10.00 },
+    {"Code":"G4", "other": "Scramble Egg", "Price": 10.00 },
+    {"Code":"G5", "other": "Chilli Chicken Porotta", "Price": 10.00 },
+    {"Code":"G6", "other": "Oman Chips Porotta", "Price": 10.00 },
+    {"Code":"G7", "other": "Tapsilog", "Price": 10.00 },
+    {"Code":"G8", "other": "Hotsilog", "Price": 10.00 },
+    {"Code":"G9", "other": "Burger", "Price": 10.00 },
+    {"Code":"G10", "other": "Shawarma", "Price": 10.00 },
+]
+
+all_items = bread + drink + fruits + noodles + soup + ceareal + chips + other #to put all the items into one variable
+store_payment = 0 # to store the payment of the customer
+store_change = 0 # to store the change of the customer
+
+print("Welcome to RISE 'N' SNACK VENDING MACHINE\n")
+print("We have the best Philippines Pastry and other breakfast food you .....\n")
+
+#using functioin to display all the items of the vending machine
+def display_items():
+    print("Here are the available items: \n")
+
+    print("Bread: \n") #using for loop to print all the bread items
+    for i in bread: 
+        print(f"Code: {i['Code']}...... Bread: {i['Bread']}...... Price: AED {i['Price']}")
+
+    print("\n Drinks: \n") #using for loop to print all the drink items
+    for i in drink: 
+        print(f"Code: {i['Code']}...... Drink: {i['Drinks']}...... Price: AED {i['Price']}")
+
+    print("\n Fruits: \n") #using for loop to print all the fruit items 
+    for i in fruits: 
+        print(f"Code: {i['Code']}...... Fruit: {i['Fruits']}...... Price: AED {i['Price']}")
+
+    print("\n Noodles: \n") #using for loop to print all the noodle items
+    for i in noodles:
+        print(f"Code: {i['Code']}...... Noodles: {i['noodles']}...... Price: AED {i['Price']}")
+
+    print("\n Soups: \n") #using for loop to print all the soup items
+    for i in soup:
+        print(f"Code: {i['Code']}...... Soup: {i['soup']}...... Price: AED {i['Price']}")
+    
+    print("\n Cereals: \n") #using for loop to print all the cereal items
+    for i in ceareal:
+        print(f"Code: {i['Code']}...... Cereal: {i['cereal']}...... Price: AED {i['Price']}")
+    
+    print("\n Chips: \n") #using for loop tp rint all the chip items
+    for i in chips:
+        print(f"Code: {i['Code']}...... Chips: {i['chips']}...... Price: AED {i['Price']}")
+
+    print("\n 10 AED MEAL (OFFER): \n") #using for loop to print all the 10dhs meal
+    for i in other:
+        print(f"Code: {i['Code']}...... Meal: {i['other']}...... Price: AED {i['Price']}")
+    
+
+#using functioin to the calculat the change of the customers payment
+def calculate_change(payment, item_price):
+        return payment - item_price
+
+#main fucntion of the vending machine
+def main():
+    display_items()#calling to print all the items
+    global store_change, store_payment #using global function to make use of both varialbe inside of the function
+    
+    while True: #using while loop to continue asking for the custoer to enter a code
+        user = input("\nPlease enter the item code or type 'q' to quit: ").lower()
+
+        if user == "q": #and typing 'q' would end the loop
+            print("\nThank you for Using the Rise 'n' Shine Vending Machine!\n")
+            break 
+
+        item_found = False #a variable to check if the input code was on the list or not
+        
+        for i in all_items: #using for loop to find the item that was inputed by the user in all_items
+            if i['Code'].lower() == user:
+                item_price = i['Price'] #a new variable to set the item price from the dictionary
+                item_type = None # a new variable to set the item type 
+
+                #using the if statement to store the item in the dictionary into item_type
+                if 'Bread' in i:
+                    item_type = i['Bread']
+                elif 'Drinks' in i:
+                    item_type = i['Drinks']
+                elif 'Fruits' in i:
+                    item_type = i['Fruits']
+                elif 'noodles' in i:
+                    item_type = i['noodles']
+                elif 'soup' in i:
+                    item_type = i['soup']
+                elif 'cereal' in i:
+                    item_type = i['cereal']
+                elif 'cereal' in i:
+                    item_type = i['cereal']
+                elif 'chips' in i:
+                    item_type = i['chips']
+                elif 'other' in i:
+                    item_type = i['other']
+                
+                print(f"The price of {item_type} is AED {item_price:.2f}.")#print the item with the price
+
+                while True: #using while loop to ask the customer for the payment and to calculate the change for the customer
+                    remaining_payment = item_price - store_change 
+                    if remaining_payment > 0: #using if statement to just ask payment once
+                        payment = float(input(f"Please enter payment: "))
+                        store_payment += payment  #storing the customers payment
+                    else:
+                        payment = 0  
+                        print("\nUsing remaining change for payment.\n")
+                    
+                    payment += store_change  #using the remaining change to pay 
+                    store_change = 0
+
+                    if payment < item_price: #using if statement to check if the custommer payed the right amount of money
+                        print("Not enough amount.... Please enter the right amount of money")
+                    else:
+                        change = calculate_change(payment, item_price)
+                        if change > 0:
+                            print(f"Your Payment is accepted. Your change is AED {change:.2f}.")
+                            store_change += change  #updating the store change variable
+                        else:
+                            print("Your Payment is accepted....")
+                        break
+                    
+                item_found = True
+                break
+        
+        if not item_found: #when the item is not found it print this statement
+            print("Invalid item code. Please try again.")
+
+# Starting the vending process
+start = input("Are you ready to buy? (yes or no): ").lower()
+if start == 'yes':
+    main()#calling the 
+    print(f"Total payment received: AED {store_payment:.2f}\n")
+    print(f"Total change given: AED {store_change:.2f}\n")
+elif start == 'no':
+    print("\n Thank you! Come back when you're ready to buy from RISE 'N' SHINE VENDING MACHINE.\n") 
+else:
+    print("\nInvalid......Please enter yes or no\n") 
+
+
